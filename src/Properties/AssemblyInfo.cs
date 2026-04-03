0e384b0cd9272aa556b0252952961f14e637a699
Klaudijus <klaudmise@gmail.com>
2026-04-03 18:09:11 +0100

    build: add test project, NUnit infrastructure, InternalsVisibleTo

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+// kmCommands (https://github.com/KMiseckas/kmCommands)
+// Copyright (c) 2026 Klaudijus Miseckas
+// Licensed under the Apache License, Version 2.0
+// See LICENSE file in the project root for full license information.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("kmCommands.Tests")]
