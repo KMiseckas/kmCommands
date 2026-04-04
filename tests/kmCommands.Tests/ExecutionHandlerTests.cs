e3477816f301330e5a5b525e470b599679f0b9c0
Klaudijus <klaudmise@gmail.com>
2026-04-05 00:37:52 +0100

    refactor(ExecutionHandlerTests): improve formatting of captured argument assertions

--- tests/kmCommands.Tests/ExecutionHandlerTests.cs
+++ tests/kmCommands.Tests/ExecutionHandlerTests.cs
@@ -162,18 +162,18 @@ namespace kmCommands.Tests
             }, args =>
             {
                 capturedString = (string)args[0];
-                capturedInt    = (int)args[1];
-                capturedFloat  = (float)args[2];
-                capturedBool   = (bool)args[3];
+                capturedInt = (int)args[1];
+                capturedFloat = (float)args[2];
+                capturedBool = (bool)args[3];
             });
 
             ExecutionResult result = _handler.Execute("cmd", new[] { "hero", "42", "1.5", "true" });
 
             Assert.That(result.Success, Is.True);
             Assert.That(capturedString, Is.EqualTo("hero"));
-            Assert.That(capturedInt,    Is.EqualTo(42));
-            Assert.That(capturedFloat,  Is.EqualTo(1.5f));
-            Assert.That(capturedBool,   Is.EqualTo(true));
+            Assert.That(capturedInt, Is.EqualTo(42));
+            Assert.That(capturedFloat, Is.EqualTo(1.5f));
+            Assert.That(capturedBool, Is.EqualTo(true));
         }
 
         [Test]
